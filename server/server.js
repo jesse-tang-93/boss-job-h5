acc6607acb70a35dc0ca5d760b0f3189a93f7866
tangxq <tangxiaoqiang@chinawbb.com>
2019-02-14 23:03:35 +0800

    添加mongoose依赖，链接mongodb。添加express依赖。

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+// 链接 mongodb 并且使用imooc这个集合。没有该集合会自动新建
+const DB_URL = 'mongodb://127.0.0.1:27017/imooc'
+mongoose.connect(DB_URL)
+mongoose.connection.on('connected',()=>{
+  console.log('mongo connect success')
+})
+// 类似于mysql的表，mongo 里面有文档、字段的概念
+
+
+const app = express()
+
+app.get('/',(req,res)=>{
+  res.send('<h1>hello wold!</h1>')
+})
+app.get('/data',(req,res)=>{
+  res.json({'name':'唐三','age':25})
+})
+app.listen(9093,()=>{
+  console.log(`node app start in port 9093`)
+})
