5e255952073d8009da512e2e4bf50b98fa5c4e08
tangxq <tangxiaoqiang@chinawbb.com>
2019-02-18 11:06:37 +0800

    以hooks形式写登录组件

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,10 +1,13 @@
-import React, {Component} from 'react'
+import React, {useState,useEffect} from 'react'
 
-class Login extends Component{
-  render(){
-    return(
-      <div>login</div>
-    )
-  }
+const Login = () => {
+  let _useState = useState(null) // 返回一个数组
+  let loginStatus = _useState[0] // 数组第一项目为初始值 null
+  let setLoginStatus =  _useState[1]
+  return (
+    <div>
+     {loginStatus}
+    </div>
+  )
 }
 export default Login
